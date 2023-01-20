c05c4769f52fedd19b8a0743c21fc5a9076def64
natividadesusana <susanajdsn@gmail.com>
2023-01-20 20:18:51 -0300

    adjusting

--- js/script.js
+++ js/script.js
@@ -1,84 +1,82 @@
 
 let message = [];
 
-let userName = {name: prompt(`🧡 Bem Vindo ao Chat UOL! 
-Digite seu nome:`)}
+let userName = {
+    name: prompt(`🧡 Bem Vindo ao Chat UOL! 
+Digite seu nome:`)
+};
+
+userNameLogin();
 
 function userNameLogin() {
-
     const promisePOST = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', userName);
     promisePOST.then(onlineUser);
     promisePOST.catch(errorUsername);
-   
+};
 
 function errorUsername(error) {
     console.log(error.response);
-    if(error.response.status === 400) {
+    if (error.response.status === 400) {
         alert(`🥴 OPS...
 Digite outro nome, pois este já está em uso!`);
-    window.location.reload()
-    }
-    if(error.response.status === 404) {
+        window.location.reload()
+    };
+    if (error.response.status === 404) {
         alert(`🥴 Erro 404 Not Found...
     Tente novamente mais tarde!`);
-    window.location.reload()
-    }
-    
-}
+        window.location.reload()
+    };
+};
 
 function sendStatus() {
     const promisePOST = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', userName);
     promisePOST.catch(() => window.location.reload())
- 
-}
+};
 
 function onlineUser() {
-   sendStatus;
-   setInterval(sendStatus, 5000);
+    sendStatus;
+    setInterval(sendStatus, 5000);
 
-   searchMessage();
-   setInterval(searchMessage, 3000);
+    searchMessage();
+    setInterval(searchMessage, 3000);
 
-   loadParticipants();
-   setInterval(loadParticipants, 1000); 
-}
+    loadParticipants();
+    setInterval(loadParticipants, 10000);
+};
 
 function searchMessage() {
     const promiseGET = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
     promiseGET.then(displayMessage);
     promiseGET.catch(() => window.location.reload());
-}
+};
 searchMessage();
 
-// Busca de participantes na sala
 function loadParticipants() {
     const promiseGET = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
     promiseGET.then(participantsFound);
     promiseGET.catch(participantsNotFound);
-}
+};
 
 function participantsFound(response) {
-    alert('Participantes Encontrados!');
     participants = response.data;
-}
+};
 
 function participantsNotFound(error) {
     alert('Erro ao buscar participantes! Atualizando página...')
     console.log(error);
     window.location.reload();
-}
-//
+};
 
 function displayMessage(response) {
     const message = response.data;
-    console.log(message)
+
     let messageContainer = document.querySelector('.message-container');
 
     messageContainer.innerHTML = '';
 
     for (let i = 0; i < message.length; i++) {
 
-        switch(message[i].type) {
+        switch (message[i].type) {
 
             case 'status':
                 messageContainer.innerHTML += `
@@ -89,7 +87,7 @@ function displayMessage(response) {
                         <span class='text'>${message[i].text}</span> 
                     </p>
                 </div>`;
-                    break;
+                break;
 
             case 'message':
                 messageContainer.innerHTML += `
@@ -116,14 +114,10 @@ function displayMessage(response) {
                     </p>
                 </div>`;
                 break;
-        }
-    }
-
-    let elementAppears = messageContainer.querySelectorAll('.scrollMessage').lastElementChild();
-    elementAppears[elementAppears.length-1].scrollIntoView();
-
-}
-// displayMessage(searchMessage);
+        };
+    };
+    messageContainer.lastChild.scrollIntoView();
+};
 
 function sendMessage() {
     const messageSent = document.querySelector('.text-input').value;
@@ -133,10 +127,10 @@ function sendMessage() {
     };
 
     const newMessage = {
-       from: userName.name,
-       to: 'Todos',
-       text: messageSent.value,
-       type: 'message'
+        from: userName.name,
+        to: 'Todos',
+        text: messageSent,
+        type: 'message'
     };
 
     const promisePOST = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', newMessage);
@@ -155,4 +149,4 @@ document.addEventListener("keypress", function (e) {
     }
 });
 
-}
+
